33e84ba72ca247496f2a0f7a21b7579e96a48194
Linux2fast <37037689+Linux2fast@users.noreply.github.com>
2025-01-17 11:50:24 +0100

    Update setup.sh
    Pfade angepasst

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 # Docker-Installationsskript herunterladen und ausführen
 echo "Installiere Docker..."
-bash <(curl -s https://raw.githubusercontent.com/<IhrGitHubBenutzername>/<IhrRepositoryName>/main/docker-install.sh)
+bash (curl -s https://raw.githubusercontent.com/Linux2fast/install/main/docker-install.sh)
 
 # Benutzererstellungs-Skript herunterladen und ausführen
 echo "Erstelle Benutzer..."
-bash <(curl -s https://raw.githubusercontent.com/<IhrGitHubBenutzername>/<IhrRepositoryName>/main/create-user.sh)
+bash (curl -s https://raw.githubusercontent.com/Linux2fast/install/main/create-user.sh)
